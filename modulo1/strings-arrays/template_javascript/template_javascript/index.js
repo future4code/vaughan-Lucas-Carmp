9dd333ed889dbc1a21cd037dff65deae44b03f03
Lucas Pasche <paschelucas9@gmail.com>
2021-12-01 14:41:43 -0300

    alteraçoes esteticas

--- modulo1/strings-arrays/template_javascript/template_javascript/index.js
+++ modulo1/strings-arrays/template_javascript/template_javascript/index.js
@@ -14,35 +14,35 @@
 
 //ESCRITA
 // exercício 1
-/* let userName = prompt("Qual o seu nome?")
+let userName = prompt("Qual o seu nome?")
 let userEmail = prompt("Qual o seu endereço de e-mail?")
-console.log(`O e-mail ${userEmail} foi cadastrado com sucesso. Seja bem vinda(o), ${userName}!`) */
+console.log(`O e-mail ${userEmail} foi cadastrado com sucesso. Seja bem vinda(o), ${userName}!`)
 
 // exercício 2
-/* let comidas = ['Pizza', 'Churrasco', 'Hambúrguer', 'Feijão vermelho', 'Macarrão do Spoleto']
+let comidas = ['Pizza', 'Churrasco', 'Hambúrguer', 'Feijão vermelho', 'Macarrão do Spoleto']
 //letra a
-console.log(comidas) */
+console.log(comidas)
 
 //letra b
-/* console.log("Essas são as minhas comidas preferidas: ")
+console.log("Essas são as minhas comidas preferidas: ")
 console.log(comidas[0])
 console.log(comidas[1])
 console.log(comidas[2])
 console.log(comidas[3])
-console.log(comidas[4]) */
+console.log(comidas[4])
 
 //letra c
-/* let comidas = ['Pizza', 'Churrasco', 'Hambúrguer', 'Feijão vermelho', 'Macarrão do Spoleto']
+let comidas = ['Pizza', 'Churrasco', 'Hambúrguer', 'Feijão vermelho', 'Macarrão do Spoleto']
 let userFood = prompt('Qual a sua comida preferida?')
 comidas.splice(1,1)
 comidas.splice(1, 0, userFood)
-console.log(comidas) */
+console.log(comidas)
 
 
 
 //exercício 3
 //letra a
-/* let listaDeTarefas = []
+let listaDeTarefas = []
 
 //letra b
 const tarefa1 = prompt('O que deve fazer pela manhã?')
@@ -62,15 +62,15 @@ const feito = prompt('Diga qual tarefa (0, 1 ou 2) você já realizou.')
 listaDeTarefas.splice(feito, 1)
 
 //letra f
-console.log(listaDeTarefas) */
+console.log(listaDeTarefas)
 
 
 
 //DESAFIOS
 //desafio 1
-/* let startPhrase = prompt('Write a sentence here.')
+let startPhrase = prompt('Write a sentence here.')
 let finalArray = startPhrase.split(' ')
-console.log(finalArray) */
+console.log(finalArray)
 
 
 //desafio 2
