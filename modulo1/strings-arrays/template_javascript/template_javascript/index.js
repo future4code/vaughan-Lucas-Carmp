063a7b338357d939ff2fdbcca5f86f698dbf7651
Lucas Pasche <paschelucas9@gmail.com>
2021-12-01 14:40:13 -0300

    exercicios da aula de arrays

---
+++ modulo1/strings-arrays/template_javascript/template_javascript/index.js
@@ -0,0 +1,87 @@
+//INTERPRETAÇÃO
+// exercício 1
+//a. undefined
+//b. null
+//c. 11
+//d. array is not defined
+//e. array is not defined
+//f. array is not defined
+
+//exercício 2
+//SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+
+
+//ESCRITA
+// exercício 1
+/* let userName = prompt("Qual o seu nome?")
+let userEmail = prompt("Qual o seu endereço de e-mail?")
+console.log(`O e-mail ${userEmail} foi cadastrado com sucesso. Seja bem vinda(o), ${userName}!`) */
+
+// exercício 2
+/* let comidas = ['Pizza', 'Churrasco', 'Hambúrguer', 'Feijão vermelho', 'Macarrão do Spoleto']
+//letra a
+console.log(comidas) */
+
+//letra b
+/* console.log("Essas são as minhas comidas preferidas: ")
+console.log(comidas[0])
+console.log(comidas[1])
+console.log(comidas[2])
+console.log(comidas[3])
+console.log(comidas[4]) */
+
+//letra c
+/* let comidas = ['Pizza', 'Churrasco', 'Hambúrguer', 'Feijão vermelho', 'Macarrão do Spoleto']
+let userFood = prompt('Qual a sua comida preferida?')
+comidas.splice(1,1)
+comidas.splice(1, 0, userFood)
+console.log(comidas) */
+
+
+
+//exercício 3
+//letra a
+/* let listaDeTarefas = []
+
+//letra b
+const tarefa1 = prompt('O que deve fazer pela manhã?')
+const tarefa2 = prompt('O que deve fazer à tarde?')
+const tarefa3 = prompt('O que deve fazer à noite?')
+listaDeTarefas.push(tarefa1)
+listaDeTarefas.push(tarefa2)
+listaDeTarefas.push(tarefa3)
+
+//letra c
+console.log(listaDeTarefas)
+
+//letra d
+const feito = prompt('Diga qual tarefa (0, 1 ou 2) você já realizou.')
+
+//letra e
+listaDeTarefas.splice(feito, 1)
+
+//letra f
+console.log(listaDeTarefas) */
+
+
+
+//DESAFIOS
+//desafio 1
+/* let startPhrase = prompt('Write a sentence here.')
+let finalArray = startPhrase.split(' ')
+console.log(finalArray) */
+
+
+//desafio 2
+let frutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+console.log(frutas.indexOf("Abacaxi"), frutas.length)
+
+
+
+
+
+
+
+
+
