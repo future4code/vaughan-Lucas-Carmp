97381eb25c1e59afb3325607a3c436988148e622
Lucas Pasche <paschelucas9@gmail.com>
2021-12-12 23:55:21 -0300

    entrega de parte do desafio. continuarei fazendo e atualizo em breve

--- modulo1/projeto-black-jack/Blackjack/desafio.js
+++ modulo1/projeto-black-jack/Blackjack/desafio.js
@@ -1,13 +1,92 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+const blackJack = () =>{
+   console.log('Welcome to Blackjack!')
+   if (confirm('Start a new game?')){
+            //creating user's hand 
+            const userCards = []
+            userCards.push(comprarCarta())
+            userCards.push(comprarCarta())
+            
+            //getting the property "valor" from the object "card"
+            const userValues = userCards.map((card) =>{
+               return Number(card.valor)
+            })
+            
+            //getting the property "texto" from the object "card"
+            const userTxt = userCards.map((card) =>{
+               return (card.texto)
+            })
+            
+            //adding the "A" conditional (step 8)
+            userTxt[0].includes("A")  && userTxt.includes("A") ? userCards.splice(0, 2, comprarCarta(), comprarCarta()) : null
+            
+            
+            
+            
+            
+            //creating cpu's hand
+            const cpuCards = []
+            cpuCards.push(comprarCarta())
+            cpuCards.push(comprarCarta())
+            
+            //getting the property "valor" from the object "card"
+            const cpuValues = cpuCards.map((card) =>{
+               return Number(card.valor)
+            })
+        
+            //getting the property "texto" from the object "card"
+            const cpuTxt = cpuCards.map((card) =>{
+               return (card.texto)
+            })
+            //adding the "A" conditional (step 8)
+            cpuTxt[0].includes("A")  && cpuTxt.includes("A") ? cpuCards.splice(0, 2, comprarCarta(), comprarCarta()) : null
+            //adding function
+            const finalValue = (array) =>{
+               let total = 0
+               for (let i = 0; i < array.length; i++) {
+               total += array[i]
+              }
+              return total;
+            }
+            
+            //step 9: drawing and showing some cards...
+            while (confirm(`Your cards are ${userTxt}. The CPU's unveiled card is ${cpuTxt[0]}. \n Do you want to draw a card?`)){
+               let userAddedValues = []
+               userAddedValues.push(comprarCarta())
+               for (let element of userAddedValues){
+                  userTxt.push(element.texto)
+                  userValues.push(element.valor)
+               }
+            } 
+
+            
+            
+            
+            
+            
+            
+            
+            
+            //creating messages...
+            let userCardValueText = `User - cards: ${userTxt} - Score ${finalValue(userValues)}`
+            let cpuCardValueText = `CPU - cards: ${cpuTxt} - Score ${finalValue(cpuValues)}`
+            let drawText = `It's a draw!`
+            let userWinText = `User's win!`
+            let cpuWinText = `CPU's win!`
+      
+            //printing messages
+            console.log(userCardValueText)
+            console.log(cpuCardValueText)
+            
+            if (finalValue(userValues) > finalValue(cpuValues)){
+               console.log(userWinText)
+            }else if (finalValue(userValues) === finalValue(cpuValues)){
+               console.log(drawText)
+            }else{
+               console.log(cpuWinText)
+            }
+   } else {
+   console.log('The game is over')
+}
+}
+blackJack() 
 
