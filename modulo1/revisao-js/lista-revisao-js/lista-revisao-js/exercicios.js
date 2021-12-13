706e9359de62dd9f97c3c5d7c7a60d8b1a4ca564
Lucas Pasche <paschelucas9@gmail.com>
2021-12-13 17:26:52 -0300

    exercicio 11 feito

--- modulo1/revisao-js/lista-revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/lista-revisao-js/exercicios.js
@@ -100,7 +100,7 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+   return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores}.` //a array de atores aparentemente foi separada sem espaço dentro das strings. por isso, é impossível retornar exatamente o que pede o boilerplate.
 }
 
 // EXERCÍCIO 12
