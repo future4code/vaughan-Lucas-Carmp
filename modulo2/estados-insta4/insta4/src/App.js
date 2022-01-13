8b9217af6da7e7822b83904f6014c0f47a47fdcc
Lucas Pasche <paschelucas9@gmail.com>
2022-01-13 18:02:41 -0300

    inicio do exercicio de array

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -10,6 +10,26 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+
+  state = {
+    posts: [
+      {
+      nomeUsuario: 'paulinha',
+      fotoUsuario: 'https://picsum.photos/50/50',
+      fotoPost:'https://picsum.photos/200/150'
+      },
+      {
+      nomeUsuario: 'fernandinho',
+      fotoUsuario: 'https://image.freepik.com/fotos-gratis/homem-trabalhador-com-sorriso-inspirado-olhando-para-a-tela-de-seu-laptop-generico-enquanto-assiste-a-um-video-online_273609-6593.jpg',
+      fotoPost:'https://www.colgate.com.br/content/dam/cp-sites/oral-care/oral-care-center/global/article/what-are-generic-drugs.jpeg'
+      },
+      {
+      nomeUsuario: 'doguinhofeliz',
+      fotoUsuario: 'https://i0.statig.com.br/bancodeimagens/2f/ym/i8/2fymi85z5vo5pcl5rsnsr3xgi.jpg',
+      fotoPost:'https://www.hypeness.com.br/1/2019/09/Vira-lata_Caramelo_1.jpg'
+      }
+    ]
+  }
   render() {
     return (
       <MainContainer>
