1ac88465df8e574abfa2a9fcee4d6ee07441a385
Lucas Pasche <paschelucas9@gmail.com>
2022-01-14 19:41:42 -0300

    estilização da página, ainda sem lógica

---
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {AppScreen} from "./components/AppScreen"
+
+
+function App() {
+  return (
+    <div className="App">
+      <AppScreen>
+  
+      </AppScreen>
+      
+    </div>
+  );
+}
+
+export default App;
