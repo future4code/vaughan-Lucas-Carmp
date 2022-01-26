17b7f669df5fb687d6f2f8fb3e04950eba468d99
Lucas Pasche <paschelucas9@gmail.com>
2022-01-26 11:18:24 -0300

    deletar funcionandogit add --all

--- modulo2/estados-insta4/insta4/src/components/IconeEnviar/IconeEnviar.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-
-export const IconeEnviar = (props) =>{
-    return <img onClick={props.onClickEnviar} src={props.icone} alt='Ícone de salvar'/>
-}
--- modulo2/estados-insta4/insta4/src/components/IconeSalvar/IconeSalvar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-
-export const IconeSalvar = (props) =>{
-    return <img onClick={props.onClickSalvar} src={props.icone} alt='Ícone de salvar'/>
-}
-
-
- 
