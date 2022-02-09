a08cfe58e50c8a43744775a4ec44ebac613708f0
Lucas Pasche <paschelucas9@gmail.com>
2022-02-08 21:45:33 -0300

    exercicio pokemon

---
+++ modulo3/astromatch/src/App.js
@@ -0,0 +1,15 @@
+import MainBackground from "./components/main-background/MainBackground";
+import "./GlobalStyle.css";
+
+
+
+function App() {
+  return (
+  <div className="page">
+      <MainBackground />
+      </div>
+
+  );
+}
+
+export default App;
