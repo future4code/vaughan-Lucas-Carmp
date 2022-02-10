f81866022094c30df905a76ac2b301c5901bf06b
Lucas Pasche <paschelucas9@gmail.com>
2022-02-10 17:41:54 -0300

    seguimos...

---
+++ modulo3/astromatch/src/background.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  width: 100%;
+  height: 15%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-family: "Lobster";
+  color: #ee4266;
+  font-weight: lighter;
+`;
