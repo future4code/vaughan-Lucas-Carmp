8a127c397f8c1645d9d417cb3a9d8861debafb53
Lucas Pasche <paschelucas9@gmail.com>
2022-03-24 17:43:16 -0300

    aprofundamento ts feito

---
+++ modulo4/aprofundamento-typescript/src/exercicio2.ts
@@ -0,0 +1,27 @@
+type amostra = {numeros: number[], obterEstatisticas: Function}
+
+function obterEstatisticas(numeros: number[]): object  {
+    type estatisticas = {maior: number, menor: number, media: number};
+
+    const numerosOrdenados = numeros.sort(
+        (a, b) => a - b
+    );
+
+    let soma: number = 0;
+
+    for (let num of numeros) {
+        soma += num;
+    };
+
+
+
+    const estatisticas: estatisticas = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    };
+
+    return estatisticas
+};
+
+console.log(obterEstatisticas([1,50,765,1000]))
