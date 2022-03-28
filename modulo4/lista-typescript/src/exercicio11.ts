6077bf27fed42e9c48cc792b4aa1ac991b45d8b5
Lucas Pasche <paschelucas9@gmail.com>
2022-03-28 00:00:12 -0300

    atualizado

--- modulo4/lista-typescript/src/exercicio11.ts
+++ modulo4/lista-typescript/src/exercicio11.ts
@@ -1,24 +1,26 @@
-const romanConversor = (num: number): string => {
-  type charElements = {
-    letter: string;
-    number: number;
-  };
+type charElements = {
+  letter: string;
+  number: number;
+};
 
-  const romanDatabase: charElements[] = [
-    { letter: "M", number: 1000 },
-    { letter: "CM", number: 900 },
-    { letter: "D", number: 500 },
-    { letter: "CD", number: 400 },
-    { letter: "C", number: 100 },
-    { letter: "XC", number: 90 },
-    { letter: "L", number: 50 },
-    { letter: "XL", number: 40 },
-    { letter: "C", number: 10 },
-    { letter: "IX", number: 9 },
-    { letter: "V", number: 5 },
-    { letter: "IV", number: 4 },
-    { letter: "I", number: 1 },
-  ];
+const romanDatabase: charElements[] = [
+  { letter: "M", number: 1000 },
+  { letter: "CM", number: 900 },
+  { letter: "D", number: 500 },
+  { letter: "CD", number: 400 },
+  { letter: "C", number: 100 },
+  { letter: "XC", number: 90 },
+  { letter: "L", number: 50 },
+  { letter: "XL", number: 40 },
+  { letter: "C", number: 10 },
+  { letter: "IX", number: 9 },
+  { letter: "V", number: 5 },
+  { letter: "IV", number: 4 },
+  { letter: "I", number: 1 },
+];
+
+const romanConversor = (num: number): string => {
+
 
   let romanYear: string = "";
 
