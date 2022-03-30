2244830723787c30478e2338cad0bd7b68b0c0af
Lucas Pasche <paschelucas9@gmail.com>
2022-03-30 19:06:27 -0300

    fluxo de dados feito

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,33 @@
+export type productType = {
+  id: string;
+  name: string;
+  price: number;
+};
+
+export const produtos: productType[] = [
+  {
+    id: "1",
+    name: "Caderno",
+    price: 35.0,
+  },
+  {
+    id: "2",
+    name: "Estojo",
+    price: 15.5,
+  },
+  {
+    id: "3",
+    name: "Lápis de cor",
+    price: 57.0,
+  },
+  {
+    id: "4",
+    name: "Borracha",
+    price: 1.5,
+  },
+  {
+    id: "5",
+    name: "Mochila",
+    price: 175.29,
+  },
+];
