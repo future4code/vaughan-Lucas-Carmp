08081361ce8b6da004e35778b193c93821e634ea
Lucas Pasche <paschelucas9@gmail.com>
2022-04-10 22:02:30 -0300

    todo list finalizado

--- modulo4/projeto-todolist/src/index.ts
+++ modulo4/projeto-todolist/src/index.ts
@@ -1,10 +1,21 @@
 import {
+  assignResponsible,
   createTask,
   createUser,
+  deleteTask,
+  deleteUser,
+  editStatus,
   editUser,
+  getDelayedTasks,
+  getResponsibles,
+  getTaskByCreatorId,
   getTaskById,
+  getTasksByStatus,
   getUserById,
   getUsers,
+  removeResponsible,
+  searchTask,
+  searchUser,
 } from "./tools/functions";
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
@@ -63,8 +74,6 @@ app.post("/tasks", async (req: Request, res: Response): Promise<void> => {
 
     await createTask(title, description, deadline, creator);
 
-    await connection("UserAndTask");
-
     res.status(201).send("Task successfully created!");
   } catch (error: any) {
     switch (error.message) {
@@ -77,13 +86,69 @@ app.post("/tasks", async (req: Request, res: Response): Promise<void> => {
   }
 });
 
+//Assigns a responsible
+app.post(
+  "/tasks/responsible",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const taskId = req.body.task_id;
+      const userId = req.body.user_id;
+
+      if (!taskId || !userId || userId.length === 0) {
+        throw new Error("Please fill in all fields.");
+      }
+
+      await assignResponsible(taskId, userId);
+
+      res.status(201).send("User assigned.");
+    } catch (error: any) {
+      switch (error.message) {
+        case "Please fill in all fields.":
+          res.status(412).send(error.message);
+          break;
+        default:
+          res
+            .status(500)
+            .send("Something bad happened. Please contact support.");
+      }
+    }
+  }
+);
+
 //Gets all users
-app.get("/users", async (req: Request, res: Response): Promise<any> => {
+app.get("/users/all", async (req: Request, res: Response): Promise<any> => {
   try {
     const response = await getUsers();
     res.status(200).send(response);
   } catch (error: any) {
     res.status(500).send("Something bad happened. Please contact support.");
+  }
+});
+
+//Searches for an user
+app.get("/users", async (req: Request, res: Response): Promise<any> => {
+  try {
+    const query = req.query.query as string;
+
+    if (!query) {
+      throw new Error("Please give a search value.");
+    }
+
+    const result = await searchUser(query);
+
+    if (result.length === 0) {
+      res.status(200).send({ users: [] });
+    }
+
+    res.status(200).send({ users: result });
+  } catch (error: any) {
+    switch (error.message) {
+      case "Please give a search value.":
+        res.status(412).send(error.message);
+        break;
+      default:
+        res.status(500).send("Something bad happened. Please contact support.");
+    }
   }
 });
 
@@ -111,6 +176,137 @@ app.get("/users/:id", async (req: Request, res: Response): Promise<any> => {
       default:
         res.status(500).send("Something bad happened. Please contact support.");
     }
+  }
+});
+
+//Gets tasks by creator id
+app.get("/tasks", async (req: Request, res: Response): Promise<any> => {
+  try {
+    const id = req.query.task_creator;
+
+    if (!id) {
+      throw new Error("Please give an Id.");
+    }
+
+    const result = await getTaskByCreatorId(id);
+
+    const formattedResult = result.tasks.map((item: any) => {
+      return {
+        ...item,
+        task_deadline: item.task_deadline
+          .toLocaleString("pt-BR")
+          .split(" ")
+          .splice(0, 1)
+          .join(),
+      };
+    });
+
+    res.status(200).send({ tasks: formattedResult });
+  } catch (error: any) {
+    switch (error.message) {
+      case "Please give an Id.":
+        res.status(412).send(error.message);
+        break;
+      default:
+        res.status(500).send("Something bad happened. Please contact support.");
+    }
+  }
+});
+
+//Searches for a task
+app.get("/tasks/search", async (req: Request, res: Response): Promise<any> => {
+  try {
+    const query = req.query.query as string;
+
+    if (!query) {
+      throw new Error("Please give a search value.");
+    }
+
+    const result = await searchTask(query);
+
+    if (result.length === 0) {
+      res.status(200).send({ tasks: [] });
+    }
+
+    const formattedResult = result.map((item: any) => {
+      return {
+        ...item,
+        task_deadline: item.task_deadline
+          .toLocaleString("pt-BR")
+          .split(" ")
+          .splice(0, 1)
+          .join(),
+      };
+    });
+
+    res.status(200).send({ tasks: formattedResult });
+  } catch (error: any) {
+    console.log(error);
+    switch (error.message) {
+      case "Please give a search value.":
+        res.status(412).send(error.message);
+        break;
+      default:
+        res.status(500).send("Something bad happened. Please contact support.");
+    }
+  }
+});
+
+//Gets tasks by status
+app.get("/tasks/status", async (req: Request, res: Response): Promise<any> => {
+  try {
+    const status = req.query.status as string;
+    if (!status) {
+      throw new Error(
+        "Please inser a valid status ('to-do', 'doing', or 'done')."
+      );
+    }
+
+    const result = await getTasksByStatus(status);
+
+    const formattedResult = result.map((item: any) => {
+      return {
+        ...item,
+        task_deadline: item.task_deadline
+          .toLocaleString("pt-BR")
+          .split(" ")
+          .splice(0, 1)
+          .join(),
+      };
+    });
+
+    res.status(200).send({ tasks: formattedResult });
+  } catch (error: any) {
+    console.log(error.message);
+    switch (error.message) {
+      case "Please inser a valid status ('to do', 'doing', or 'done').":
+        res.status(412).send(error.message);
+        break;
+      default:
+        res.status(500).send("Something bad happened. Please contact support.");
+    }
+  }
+});
+
+//Gets overdue tasks
+app.get("/tasks/overdue", async (req: Request, res: Response): Promise<any> => {
+  try {
+    const result = await getDelayedTasks();
+
+    const formattedResult = result.map((item: any) => {
+      return {
+        ...item,
+        task_deadline: item.task_deadline
+          .toLocaleString("pt-BR")
+          .split(" ")
+          .splice(0, 1)
+          .join(),
+      };
+    });
+
+    res.status(200).send({ tasks: formattedResult });
+  } catch (error) {
+    console.log(error);
   }
 });
 
@@ -147,7 +343,47 @@ app.get("/tasks/:id", async (req: Request, res: Response): Promise<any> => {
   }
 });
 
-//Edit user
+//Gets responsibles for an specific task by task's id
+app.get(
+  "/tasks/:id/responsible",
+  async (req: Request, res: Response): Promise<any> => {
+    try {
+      const tasks = await connection("Tasks").select();
+      const id: string = req.params.id;
+
+      const idList = tasks.map((task) => {
+        return task.task_id;
+      });
+
+      if (!id) {
+        throw new Error("Please give an Id.");
+      }
+
+      if (!idList.includes(id)) {
+        throw new Error("Task not found.");
+      }
+
+      const result = await getResponsibles(id);
+
+      res.status(200).send(result);
+    } catch (error: any) {
+      switch (error.message) {
+        case "Please give an Id":
+          res.status(412).send(error.message);
+          break;
+        case "Task not found.":
+          res.status(404).send(error.message);
+          break;
+        default:
+          res
+            .status(500)
+            .send("Something bad happened. Please contact support.");
+      }
+    }
+  }
+);
+
+//Edits user
 app.patch(
   "/users/edit/:id",
   async (req: Request, res: Response): Promise<any> => {
@@ -203,6 +439,157 @@ app.patch(
     }
   }
 );
+
+//Edits status
+app.patch(
+  "/tasks/status/edit",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const tasks = await connection("Tasks").select("task_id");
+      const id = req.body.id;
+      const status = req.body.status;
+
+      if (id.length === 0) {
+        throw new Error("Please give an Id.");
+      }
+
+      const taskIds = tasks.map((item) => {
+        return item.task_id;
+      });
+      id.forEach((item: any) => {
+        if (!taskIds.includes(item)) {
+          throw new Error("Task not found.");
+        }
+      });
+
+      if (!status) {
+        throw new Error(
+          "Please give a valid status('to-do', 'doing' or 'done')."
+        );
+      }
+
+      await editStatus(id, status);
+
+      res.status(200).send("Status successfully updated!");
+    } catch (error: any) {
+      console.log;
+      switch (error.message) {
+        case "Please give an Id.":
+          res.status(412).send(error.message);
+          break;
+        case "Task not found.":
+          res.status(404).send(error.message);
+          break;
+        case "Please give a valid status('to do', 'doing' or 'done').":
+          res.status(412).send(error.message);
+          break;
+        default:
+          res
+            .status(500)
+            .send("Something bad happened. Please contact support.");
+      }
+    }
+  }
+);
+
+//Delete task
+app.delete("/tasks/:id", async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id = req.params.id;
+
+    if (!id) {
+      throw new Error("Please give an Id.");
+    }
+
+    await deleteTask(id);
+    res.status(200).send("Task deleted.");
+  } catch (error: any) {
+    switch (error.message) {
+      case "Please give an Id.":
+        res.status(412).send(error.message);
+        break;
+      default:
+        res.status(500).send("Something bad happened. Please contact support.");
+    }
+  }
+});
+
+//Remove responsible
+app.delete(
+  "/tasks/:taskId/responsible/:userId",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const taskId = req.params.taskId;
+      const userId = req.params.userId;
+      const taskUserPair = await connection("UserAndTask")
+        .select()
+        .where("task_id", taskId);
+
+      if (!taskId || !userId) {
+        throw new Error("Task or user id missing.");
+      }
+
+      const taskIdList = taskUserPair.map((item) => {
+        return item.task_id;
+      });
+      const userIdList = taskUserPair.map((item) => {
+        return item.user_id;
+      });
+
+      if (!taskIdList.includes(taskId)) {
+        throw new Error("Task not found.");
+      }
+
+      if (!userIdList.includes(userId)) {
+        console.log(taskUserPair, userId);
+        throw new Error("This user isn't responsible for this task.");
+      }
+
+      await removeResponsible(taskId, userId);
+
+      res.status(200).send("Responsible removed.");
+    } catch (error: any) {
+      switch (error.message) {
+        case "Task or user id missing.":
+          res.status(412).send(error.message);
+          break;
+        case "User or task not found.":
+          res.status(404).send(error.message);
+          break;
+        case "This user isn't responsible for this task.":
+          res.status(412).send(error.message);
+          break;
+        default:
+          res
+            .status(500)
+            .send("Something bad happened. Please contact support.");
+      }
+    }
+  }
+);
+
+//Delete user
+app.delete("/users/:id", async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+      throw new Error("Please give an Id.");
+    }
+
+    await deleteUser(id);
+
+    res.status(200).send("User deleted.");
+  } catch (error: any) {
+    console.log(error.message);
+    switch (error.message) {
+      case "Please give an Id.":
+        res.status(412).send(error.message);
+        break;
+      default:
+        res.status(500).send("Something bad happened. Please contact support.");
+    }
+  }
+});
 
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
--- modulo4/projeto-todolist/src/tools/functions.ts
+++ modulo4/projeto-todolist/src/tools/functions.ts
@@ -34,12 +34,37 @@ export const createTask = async (
     .into("Tasks");
 };
 
+//Assigns an user to a task
+export const assignResponsible = async (
+  taskId: string,
+  userId: string[]
+): Promise<void> => {
+  userId.forEach(async (item: string) => {
+    await connection("UserAndTask")
+      .insert({
+        task_id: taskId,
+        user_id: item,
+      })
+      .into("UserAndTask");
+  });
+};
+
 //Gets all users
 export const getUsers = async (): Promise<any> => {
   const users: any = [];
   const result = await connection("Users").select("user_id", "user_nick");
   users.push(result);
   return { users: users.flat(1) };
+};
+
+//Searches for an user
+export const searchUser = async (query: string): Promise<any> => {
+  const result = await connection("Users")
+    .select("user_id", "user_nick")
+    .where("user_nick", "like", `%${query}%`)
+    .orWhere("user_email", "like", `%${query}%`);
+
+  return result;
 };
 
 //Gets user by id
@@ -51,22 +76,117 @@ export const getUserById = async (id: string): Promise<any> => {
   return response[0];
 };
 
+//Gets tasks by creator id
+export const getTaskByCreatorId = async (id: any): Promise<any> => {
+  const tasks = [];
+
+  const result = await connection("Tasks")
+    .join("Users", "Users.user_id", "Tasks.task_creator")
+    .select(
+      "Tasks.task_id",
+      "Tasks.task_title",
+      "Tasks.task_description",
+      "Tasks.task_deadline",
+      "Tasks.task_creator",
+      "Tasks.task_status",
+      "Users.user_nick"
+    )
+    .where("Tasks.task_creator", id);
+
+  tasks.push(result);
+
+  return { tasks: result.flat(1) };
+};
+
+//Searches for a task
+export const searchTask = async (query: string): Promise<any> => {
+  const result = await connection("Tasks")
+    .join("Users", "Users.user_id", "Tasks.task_creator")
+    .select(
+      "task_id",
+      "task_title",
+      "task_description",
+      "task_deadline",
+      "task_creator",
+      "Users.user_nick"
+    )
+    .where("task_title", "like", `%${query}%`)
+    .orWhere("task_description", "like", `%${query}%`);
+
+  return result;
+};
+
+//Gets tasks by status
+export const getTasksByStatus = async (status: string): Promise<any> => {
+  let taskStatus = status;
+  if (status === "to-do") {
+    taskStatus = status.split("-").join(" ");
+  }
+
+  const result = await connection("Tasks")
+    .join("Users", "Users.user_id", "Tasks.task_creator")
+    .select(
+      "Tasks.task_id",
+      "Tasks.task_title",
+      "Tasks.task_description",
+      "Tasks.task_deadline",
+      "Tasks.task_creator",
+      "Users.user_nick"
+    )
+    .where("Tasks.task_status", taskStatus);
+
+  return result;
+};
+
+//Gets delayed tasks
+export const getDelayedTasks = async (): Promise<any> => {
+  const now = new Date(Date.now());
+  const result = await connection("Tasks")
+    .join("Users", "Users.user_id", "Tasks.task_creator")
+    .select(
+      "Tasks.task_id",
+      "Tasks.task_title",
+      "Tasks.task_description",
+      "Tasks.task_deadline",
+      "Tasks.task_creator",
+      "Users.user_nick"
+    )
+    .where("Tasks.task_deadline", "<", now);
+
+  return result;
+};
+
 //Gets task by id
 export const getTaskById = async (id: string): Promise<any> => {
-  const response = await connection("Tasks").select(
-    "task_id", 
-    "task_title",
-    "task_description",
-    "task_status",
-    "task_deadline",
-    "task_creator"
-  )
-  .where("task_id", id);
+  const response = await connection("Tasks")
+    .select(
+      "task_id",
+      "task_title",
+      "task_description",
+      "task_status",
+      "task_deadline",
+      "task_creator"
+    )
+    .where("task_id", id);
 
   return response[0];
 };
 
-//Edit user
+//Gets all responsibles by task
+export const getResponsibles = async (id: string): Promise<any> => {
+  const responsibles: any = [];
+
+  const result = await connection("Users")
+    .join("UserAndTask", "Users.user_id", "UserAndTask.user_id")
+    .select("Users.user_id", "Users.user_nick")
+    .where("UserAndTask.task_id", id);
+
+  responsibles.push(result);
+
+  return { responsibles: responsibles.flat(1) };
+};
+
+//Edits user
 export const editUser = async (
   id: string,
   name: string,
@@ -78,4 +198,54 @@ export const editUser = async (
       user_nick: nick,
     })
     .where("user_id", id);
+};
+
+//Edits status
+export const editStatus = async (
+  id: string[],
+  status: string
+): Promise<void> => {
+  id.forEach(async (item: string) => {
+    await connection("Tasks")
+      .update("task_status", status)
+      .where("task_id", item);
+  });
+};
+
+//Deletes a task
+export const deleteTask = async (id: string): Promise<void> => {
+  const responsiblesResult = await getResponsibles(id);
+  responsiblesResult.responsibles
+    .map((item: any) => {
+      return item.user_id;
+    })
+    .forEach((userId: string) => {
+      removeResponsible(id, userId);
+    });
+
+  await connection("Tasks").where("task_id", id).del();
+};
+
+//Removes responsible
+export const removeResponsible = async (
+  taskId: string,
+  userId: string
+): Promise<void> => {
+  await connection("UserAndTask")
+    .where("user_id", userId)
+    .andWhere("task_id", taskId)
+    .del();
+};
+
+//Deletes an user
+export const deleteUser = async (userId: string): Promise<void> => {
+  const user = await connection("UserAndTask")
+    .where("user_id", userId)
+    .select();
+
+  user.forEach(async (item) => {
+    removeResponsible(item.task_id, userId);
+  });
+  await connection("Tasks").where("task_creator", userId).del();
+  await connection("Users").where("user_id", userId).del();
 };
